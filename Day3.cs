a530c2986913a28069bbee8f4c6bb5165fb97b7e
DEnyy11 <daniel.vasut2@email.cz>
2025-01-31 09:44:22 +0100

    day 3 part 2 added

--- Day3.cs
+++ Day3.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2024 {
     internal class Day3 {
@@ -7,19 +9,37 @@ namespace AdventOfCode2024 {
             string input = File.ReadAllText(filePath);
 
             string pattern = @"mul\((\d{1,3}),\s*(\d{1,3})\)";
-            Regex regex = new Regex(pattern);
+            string doPattern = @"do\(\)";
+            string dontPattern = @"don't\(\)";
 
-            MatchCollection matches = regex.Matches(input);
+            Regex mulRegex = new Regex(pattern);
+            Regex doRegex = new Regex(doPattern);
+            Regex dontRegex = new Regex(dontPattern);
 
             int totalSum = 0;
+            bool enabled = true;
 
-            foreach (Match match in matches) {
-                int number1 = int.Parse(match.Groups[1].Value);
-                int number2 = int.Parse(match.Groups[2].Value);
-                int sum = number1 * number2;
-                totalSum += sum;
+            for (int i = 0; i < input.Length; i++) {
+                if (input.Substring(i).StartsWith("do()")) {
+                    enabled = true;
+                    i += 3; //do delka 4 znaky
+                }
+                else if (input.Substring(i).StartsWith("don't()")) {
+                    enabled = false;
+                    i += 6; //dont delka 7 znaku
+                }
+                else {
+                    Match match = mulRegex.Match(input, i);
+                    if (match.Success && match.Index == i) {
+                        if (enabled) {
+                            int number1 = int.Parse(match.Groups[1].Value);
+                            int number2 = int.Parse(match.Groups[2].Value);
+                            totalSum += number1 * number2;
+                        }
+                        i += match.Length - 1; 
+                    }
+                }
             }
-
             Console.WriteLine(totalSum);
         }
     }
